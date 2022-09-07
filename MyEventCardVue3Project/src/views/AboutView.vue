c520da01bd57a46968f6b50ddaa68bef92ec757a
freedombridget <86961410+freedombridget@users.noreply.github.com>
2022-09-07 07:40:48 +0100

    initial commit

---
+++ MyEventCardVue3Project/src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <p>A site for events to better the world.</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
